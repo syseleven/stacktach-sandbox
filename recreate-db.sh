a72a40a9c608157846e17640dd3aba8cbddb8ef7
Olaf Klischat <o.klischat@syseleven.de>
2016-10-06 11:37:38 +0200

    recrete-db: create schema

--- recreate-db.sh
+++ recreate-db.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-# TODO: largely c&p from vagrant/bootstrap.sh, change: no root passwd
+cd "$(dirname $0)"
+
+# TODO: largely c&p from vagrant/bootstrap.sh, changes: no root passwd, db scheme create
 
 echo "drop database if exists winchester" | mysql -uroot
 echo "DROP USER 'winchester'@'localhost'" | mysql -uroot 2>/dev/null || true
@@ -10,3 +12,5 @@ echo "CREATE USER 'winchester'@'localhost' IDENTIFIED BY 'testpasswd'" | mysql -
 echo "CREATE DATABASE winchester" | mysql -uroot
 echo "GRANT ALL ON winchester.* TO 'winchester'@'localhost'" | mysql -uroot
 echo "flush privileges" | mysql -uroot
+
+. ./.venv/bin/activate && winchester_db -c winchester.yaml upgrade head
